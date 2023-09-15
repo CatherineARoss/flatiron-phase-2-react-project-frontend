76778ca3b61b82984623e8b18dde48f1893e36b8
Catherine Ross <fibervibes@gmail.com>
2023-09-15 17:35:09 -0400

    adds a Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>Fiber Vibes</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#some-id">Link Title</a></li>
+                        <li><a href="#some-id">Link Title</a></li>
+                        <li><a href="#some-id">Link Title</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
