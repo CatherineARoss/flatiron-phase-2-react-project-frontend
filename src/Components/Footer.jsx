bb2fa90ed07e8cfc341c6110d1fa88ba0e074fe8
Catherine Ross <fibervibes@gmail.com>
2023-09-15 18:13:58 -0400

    removes css from main.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
             <footer>
                 <ul>
                     <li><a href="#top">Go to top</a></li>
-                    <li><a href="#some-id2" target="_blank">Link Title 2</a></li>
-                    <li><a href="#some-id3" target="_blank">Link Title 3</a></li>
+                    <li><a href="#some-id2" target="_blank">Social Media Link 2</a></li>
+                    <li><a href="#some-id3" target="_blank">Social Media Link 3</a></li>
                 </ul>
             </footer>
         </>
