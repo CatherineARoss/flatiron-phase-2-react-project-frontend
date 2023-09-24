8b941eef206ed114cbeafc8a9c3380717bd0275d
Catherine Ross <fibervibes@gmail.com>
2023-09-24 16:45:39 -0400

    adds a form in the footer along with an event listener to prevent deafault when submitted

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,46 @@
+import React from 'react';
+
 const Footer = () => {
+
+    const emailElement = document.querySelector('#email');
+const messageElement = document.querySelector('#message');
+const submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const emailValue = emailElement.value;
+  const messageValue = messageElement.value;
+
+  console.log('Email: ', emailValue);
+  console.log('Message: ', messageValue);
+
+  if (emailValue.includes('@')) {
+    alert('Thank You');
+  } else {
+    alert('Please enter a valid email address.');
+  }
+});
+
     return (
-        <>
-            <footer>
-                <ul>
-                    <li><a href="#top">Go to top</a></li>
-                    <li><a href="#some-id2" target="_blank">Social Media Link 2</a></li>
-                    <li><a href="#some-id3" target="_blank">Social Media Link 3</a></li>
-                </ul>
-            </footer>
-        </>
+        <footer>
+            <div className="contact-us">
+                <h4>Contact Us</h4>
+                <form>
+                    <label htmlFor="email">
+                        Email:
+                        <input id="email" type="email" placeholder="Enter your Email" />
+                    </label>
+                    <br></br>
+                    <label htmlFor="message">
+                        Ask a question:
+                        <textarea id="message" placeholder="Your Message"></textarea>
+                    </label>
+                    <div className="submit-button-wrapper">
+                        <button id="submit-button" type="submit" value="Submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </footer>
     )
 }
 
