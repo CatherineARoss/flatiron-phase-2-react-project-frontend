d20c76a0ff91e24fd002b81bb841700653ec9cb0
Catherine Ross <fibervibes@gmail.com>
2023-09-27 12:49:19 -0400

    sets up NanLinks in the the NavBar component and the Routes in App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import Main from './Main';
 import Footer from './Footer';
-import '../styles.css'
-
+import Home from './Home'; 
+import About from './About'; 
+import Cart from './Cart'; 
+import '../styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <NavBar />
+      <Switch>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/" component={Home} />
+      </Switch>
       <Main />
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
 
 const NavBar = () => {
-    return (
-        <header>
-            <h1 className="site-title">Fiber Vibes</h1>
-                <nav className="nav">  
-                    <a href='/'>Home</a>
-                    <a href='/about'>About</a>
-                    <a href='/cart'>Cart</a>
-                </nav>
-        </header>
-    
-    )
-}
+  return (
+    <header>
+      <h1 className="site-title">Fiber Vibes</h1>
+      <nav className="nav">
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/about" exact>
+          About
+        </NavLink>
+        <NavLink to="/cart" exact>
+          Cart
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
