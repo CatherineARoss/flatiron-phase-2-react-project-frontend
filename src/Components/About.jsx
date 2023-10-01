d2bc3729bb8604d3a3206d64dd2b6c4ff4d7984a
Catherine Ross <fibervibes@gmail.com>
2023-10-01 12:49:06 -0400

    changes info displayed in About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,14 +8,22 @@ const About = () => {
             <img className="about-photo" src={MeetTheMaker} />
             <h2>Handmade Home Goods & Clothing for Those Wanting a Slower Lifestyle.</h2>
             <hr></hr>
-            <p>My name is Catherine and I am a true nature lover from Oregon who just moved to Georgia. 
-                On sunny days you can find me back-packing and kayaking with my Collie dog. 
-                I have been crocheting since a young age when I was taught by my aunts, 
-                and have spent many hours passionately creating cozy items to wear and use in my home.</p>
+            <p>My name is Catherine, and I'm a true nature lover originally from the scenic landscapes of Oregon. 
+                Recently, I embarked on a new adventure and made a move to the beautiful state of Georgia. 
+                On those blissful, sunny days, you'll often find me immersed in the great outdoors, either backpacking 
+                through lush trails or gliding along serene waters in my kayak, always in the delightful company 
+                of my Collie dog.</p>
             <br></br>
-            <p>Colors influenced by nature are my favorite to use since they have a grounding effect and remind me of memories spent outside. 
-                My inspiration comes from my love of living a slow and simple lifestyle, 
-                and it brings me great joy to share this with you.</p>
+            <p>From a young age, the art of crocheting was taught to me by my beloved aunts. 
+                Those early lessons sparked a lifelong passion for crafting, 
+                and I've since dedicated countless hours to creating snuggly, 
+                comforting pieces to wear and decorate my home with.</p>
+            <br></br>
+            <p>Colors that echo the beauty of nature resonate deeply with me; 
+                they have an incredible grounding effect and never fail to transport me back to 
+                cherished memories spent amidst the natural world. Drawing inspiration from 
+                my love for a slow and simple lifestyle, my creations exude warmth and authenticity. 
+                It's my genuine delight to share this joy and tranquility with you through my handmade items.</p>
         </div>
     )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
               <div className="home">
                 <h2>Handmade Home Decor, Clothing, and Crochet & Knit Patterns</h2>
                 <p><span>&#9733;&#9733;&#9733;&#9733;&#9733;</span>5 Stars</p>
-                <p>1,782 Sales</p>
+                <p>1,791 Sales</p>
               </div>
               <hr></hr>
                 <FiberVibesListings />
